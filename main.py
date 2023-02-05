0db4c820f67fb21da4cd49aa834d9a72d7951d66
Ariel Villafane <ariel.g.villafane@gmail.com>
2023-02-05 15:55:48 -0300

    second commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import req
+
+def run():
+    req.get_from_page()
+
+if __name__=='__main__':
+    run()
---
+++ req.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_from_page():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print('------------')
+    if r.status_code == 200: 
+        print('200 Means everything was fine')
+    else:
+        print('something went wrong with the link')
+    print(r.text)
+    
